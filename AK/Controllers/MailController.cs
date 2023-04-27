73ee02c60e174bee64eb0aefa7bd409ca2fe8071
AKASH-k-s63 <109033257+AKASH-k-s63@users.noreply.github.com>
2023-04-27 17:22:05 +0530

    code cleaned

---
+++ AK/Controllers/MailController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+
+namespace AK.Controllers
+{
+    public class MailController : Controller
+    {
+        private readonly ILogger<MailController> _logger;
+
+        public MailController(ILogger<MailController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Mail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("akashks63697@gmail.com", "Akash");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "PODAPANNIpayalae";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                //ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
+
+    }
+}
